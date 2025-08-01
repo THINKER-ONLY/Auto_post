23dbca44f918e392b364a791f6fca38cd8101d50
THINKER-ONLY <3193304954@qq.com>
2025-08-01 14:38:17 +0800

    [update]更新了视频号的推送机制，设置为videos目录下的每个子目录单独存放单个视频并单独推送

--- upload_video_to_tencent.py
+++ upload_video_to_tencent.py
@@ -6,7 +6,7 @@ from uploader.tencent_uploader.main import weixin_setup, TencentVideo
 from utils.constant import TencentZoneTypes
 from utils.files_times import generate_schedule_time_next_day, get_title_and_hashtags, generate_schedule_time
 
-
+"""
 if __name__ == '__main__':
     filepath = Path(BASE_DIR) / "videos"
     account_file = Path(BASE_DIR / "cookies" / "tencent_uploader" / "account.json")
@@ -36,3 +36,28 @@ if __name__ == '__main__':
         app = TencentVideo(title, file, tags, publish_datetimes, account_file, category)
         #app = TencentVideo(title, file, tags, 0, account_file, category)
         asyncio.run(app.main(), debug=False)
+"""
+
+if __name__ == "__main__" :
+    folderpath = Path(BASE_DIR) / "videos"
+    folder_path = Path(folderpath)
+    #folders = list(folder_path.glob("*"))
+    folders = [f for f in folder_path.glob("*") if f.is_dir()]
+    for num in range(len(folders)) :
+        filepath_son = Path(BASE_DIR) / "videos" + f"_{num}"
+        account_file = Path(BASE_DIR / "cookies" / "tencent_uploader" / "account.json")
+        folder_path_son = Path(filepath_son)
+        files_son = list(folder_path_son.glob("*.mp4"))
+        file_num_son = len(files_son)
+        publish_datetimes = generate_schedule_time(file_num_son, file_num_son, daily_times=[6, 10, 16])
+        cookie_setup = asyncio.run(weixin_setup(account_file, handle=True))
+        category = TencentZoneTypes.LIFESTYLE.value 
+        for file in enumerate(files_son):
+            title, tags = get_title_and_hashtags(str(file))
+            # 打印视频文件名、标题和 hashtag
+            print(f"视频文件名：{file}")
+            print(f"标题：{title}")
+            print(f"Hashtag：{tags}")
+            app = TencentVideo(title, file, tags, publish_datetimes, account_file, category)
+            #app = TencentVideo(title, file, tags, 0, account_file, category)
+            asyncio.run(app.main(), debug=False)
